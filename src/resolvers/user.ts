bd08883f6168e1d3d01a226465434e0b1f5e9649
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-14 21:55:54 -0300

    Session Authentication

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -31,10 +31,23 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, { nullable: true })
+    async me (
+        @Ctx() {req, em}: MyContext
+    ) {
+        // You are not logged in
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId })
+        return user;
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse> {
         if (options.username.length <= 2) {
             return {
@@ -65,15 +78,29 @@ export class UserResolver {
         try {
             await em.persistAndFlush(user);
         } catch(err) {
+            if (err.code === '23505' || err.detail.includes('already exists')) {
+                // duplicate username error
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'username already taken'
+                        },
+                    ],
+                };
+            };
             console.log('error: ' + err.message);
         }
+
+        req.session.userId = user.id;
+
         return { user };
     }
 
     @Mutation(() => UserResponse)
     async login(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse> {
         const user = await em.findOne(User, { username: options.username });
         if (!user) {
@@ -94,6 +121,7 @@ export class UserResolver {
             };
         }
 
+        req.session.userId = user.id;
 
         return {
             user
