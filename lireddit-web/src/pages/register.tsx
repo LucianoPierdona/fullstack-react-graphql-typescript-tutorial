0b3752efae565e4a7ea523e1af9a4b988228ded2
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-16 15:15:08 -0300

    urql basics, graphql code generator, register error handling

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -3,15 +3,49 @@ import { Formik, Form } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { Box, Button } from '@chakra-ui/core';
+import { useMutation } from 'urql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from "next/router"
 
 interface registerProps {
 
 }
 
+const REGISTER_MUTATION = `
+  mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+
 const Register: React.FC<registerProps> = ({}) => {
+    const router = useRouter();
+    const [,register] = useMutation(REGISTER_MUTATION);
+
     return (
       <Wrapper variant="small">
-        <Formik initialValues={{ username: "", password: "" }} onSubmit={(values) => console.log(values)}>
+        <Formik 
+          initialValues={{ username: "", password: "" }} 
+          onSubmit={async (values, {setErrors}) => {
+            const response = await register(values);
+            if (response.data?.register.errors) {
+              setErrors({
+                setErrors(toErrorMap)
+              })
+            } else if (response.data?.register.user) {
+              // worked
+              router.push('/');
+            }
+          }}
+        >
           {() => (
             <Form>
                 <InputField
