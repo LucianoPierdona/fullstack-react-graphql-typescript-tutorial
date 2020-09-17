1653217dab9e4cda2f772e69cf72e1e56b3cd9af
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-17 17:45:11 -0300

    switching to typeorm pt1

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,22 +1,31 @@
+import "reflect-metadata";
 import { COOKIE_NAME, __prod__ } from './constants';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import "reflect-metadata";
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
-import { MikroORM } from '@mikro-orm/core';
-import mikroOrmConfig from './mikro-orm.config';
 import { UserResolver } from './resolvers/user';
 import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroOrmConfig);
-    await orm.getMigrator().up();
+    const conn = await createConnection({
+        type: 'postgres',
+        database: 'lireddit2',
+        username: 'postgres',
+        password: 'postgres',
+        logging: true,
+        synchronize: true,
+        entities: [Post, User],
+    });
 
+    console.log(conn)
     const app = express();
 
     const RedisStore = connectRedis(session);
@@ -52,7 +61,7 @@ const main = async () => {
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({ req, res }) => ({ em: orm.em, req, res, redis })
+        context: ({ req, res }) => ({ req, res, redis })
     });
 
     apolloServer.applyMiddleware({
--- lireddit-server/src/resolvers/post.ts
+++ lireddit-server/src/resolvers/post.ts
@@ -1,61 +1,46 @@
-import { MyContext } from "src/types";
-import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 
 @Resolver()
 export class PostResolver {
-    // Get all posts
-    @Query(() => [Post])
-    posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-        return em.find(Post, {});
-    }
-    
-    // Get one post
-    @Query(() => Post, { nullable: true })
-    post(
-        @Arg('id') id: number,
-        @Ctx() { em }: MyContext
-    ): Promise<Post | null> {
-        return em.findOne(Post, { id });
-    }
+  // Get all posts
+  @Query(() => [Post])
+  async posts(): Promise<Post[]> {
+    return Post.find();
+  }
 
+  // Get one post
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number): Promise<Post | undefined> {
+    return Post.findOne(id);
+  }
 
-    // Create a new post
-    @Mutation(() => Post)
-    async createPost(
-        @Arg('title') title: string,
-        @Ctx() { em }: MyContext
-    ): Promise<Post> {
-        const post = em.create(Post, {title})
-        await em.persistAndFlush(post);
-        return post
-    }
-    
-    // Update a post
-    @Mutation(() => Post, { nullable: true })
-    async updatePost(
-        @Arg('id') id: number,
-        @Arg('title', () => String, { nullable: true }) title: string,
-        @Ctx() { em }: MyContext
-    ): Promise<Post | null> {
-        const post = em.findOne(Post, {id});
-        if (!post) {
-            return null
-        }
-        if (typeof title !== 'undefined') {
-            await em.persistAndFlush(post); // post.title = title
-        }
-        return post
-    }
+  // Create a new post
+  @Mutation(() => Post)
+  async createPost(@Arg("title") title: string): Promise<Post> {
+    return Post.create({ title }).save();
+  }
 
-
-    // Delete a post
-    @Mutation(() => Boolean)
-    async deletePost(
-        @Arg('id') id: number,
-        @Ctx() { em }: MyContext
-    ): Promise<boolean> {
-        await em.nativeDelete(Post, { id });
-        return true;
+  // Update a post
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string
+  ): Promise<Post | null> {
+    const post = await Post.findOne(id);
+    if (!post) {
+      return null;
     }
+    if (typeof title !== "undefined") {
+      await Post.update({ id }, { title });
+    }
+    return post;
+  }
+
+  // Delete a post
+  @Mutation(() => Boolean)
+  async deletePost(@Arg("id") id: number): Promise<boolean> {
+    await Post.delete(id);
+    return true;
+  }
 }
