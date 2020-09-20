e5a70e24978fff2740016909ad406a5e15174885
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-20 15:08:38 -0300

    chakra styling

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
@@ -6,19 +6,44 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
 
+  if (!fetching && !data) {
+    return <div>You got query failed for some reason</div>;
+  }
+
   return (
     <Layout>
       <NavBar />
-      <NextLink href="/create-post">
-        <Link>create post</Link>
-      </NextLink>
-      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
+      {!data && fetching ? (
+        <div>loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
