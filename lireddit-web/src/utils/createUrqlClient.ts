709dbaf9216a90a4b8a99af3e79e96b38cb726c8
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-20 18:06:42 -0300

    urql pagination has more

--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -35,18 +35,32 @@ const cursorPagination = (): Resolver => {
     }
     const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
 
-    const isItInTheCache = cache.resolveFieldByKey(entityKey, fieldKey);
+    const isItInTheCache = cache.resolve(
+      cache.resolveFieldByKey(entityKey, fieldKey) as string,
+      "posts"
+    );
 
     info.partial = !isItInTheCache;
+
+    let hasMore = true;
 
     const results: string[] = [];
 
     fieldInfos.forEach((fi) => {
-      const data = cache.resolveFieldByKey(entityKey, fi.fieldKey) as string[];
+      const key = cache.resolveFieldByKey(entityKey, fi.fieldKey) as string;
+      const data = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore");
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
       results.push(...data);
     });
 
-    return results;
+    return {
+      __typename: "PaginatedPosts",
+      hasMore, // true or false
+      posts: results,
+    };
 
     // const visited = new Set();
     // let result: NullArray<string> = [];
@@ -110,6 +124,9 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PaginatedPosts: () => null,
+      },
       resolvers: {
         Query: {
           posts: cursorPagination(),
