3652aa0c52235cbe63127958bdbcfec947a7a99f
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-22 01:06:17 -0300

    ssr cookie forwarding

--- lireddit-web/src/components/NavBar.tsx
+++ lireddit-web/src/components/NavBar.tsx
@@ -1,50 +1,45 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/core';
-import React from 'react';
-import NextLink from 'next/link';
-import { isServer } from '../utils/isServer';
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import React from "react";
+import NextLink from "next/link";
 
-interface NavBarProps {
-
-}
+interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-    const [{fetching: logoutFetching}, logout] = useLogoutMutation();
-    const [{data, fetching}] = useMeQuery({
-        pause: isServer(),
-    });
-    let body = null;
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
 
-    // data is loading
-    if (fetching) {
-        body = null;
-    } else if (!data?.me) { // user not logged in
-        body = (
-            <>
-                <NextLink href="/login">
-                    <Link color="white" mr={2}>
-                        Login
-                    </Link>
-                </NextLink>
-                <NextLink href="/register">
-                    <Link color="white">
-                        Register
-                    </Link>
-                </NextLink>
-            </>
-        )
-    } else { // user is logged in
-        body = (
-            <Flex>
-                <Box mr={2}>{data.me.username}</Box>
-                <Button onClick={logout} isLoading={logoutFetching} variant="link">Logout</Button>
-            </Flex>
-        )
-    }
-    return (
-        <Flex zIndex={1} position="sticky" top={0} bg="tomato" p={4}>
-            <Box ml={'auto'}>
-                {body}
-            </Box>
-        </Flex>
+  // data is loading
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    // user not logged in
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </>
     );
-}
+  } else {
+    // user is logged in
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button onClick={logout} isLoading={logoutFetching} variant="link">
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex zIndex={1} position="sticky" top={0} bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -10,6 +10,7 @@ import { pipe, tap } from "wonka";
 import { MeDocument } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 import gql from "graphql-tag";
+import { isServer } from "./isServer";
 
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
   return pipe(
@@ -117,109 +118,121 @@ const cursorPagination = (): Resolver => {
   };
 };
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include" as const,
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      keys: {
-        PaginatedPosts: () => null,
-      },
-      resolvers: {
-        Query: {
-          posts: cursorPagination(),
-        },
-      },
-      updates: {
-        Mutation: {
-          vote: (_result, args, cache, info) => {
-            const { postId, value } = args as VoteMutationVariables;
-            const data = cache.readFragment(
-              gql`
-                fragment _ on Post {
-                  id
-                  points
-                  voteStatus
-                }
-              `,
-              { id: postId } as any
-            );
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx.req.headers.cookie;
+  }
 
-            if (data) {
-              if (data.voteStatus === value) {
-                return;
-              }
-              const newPoints =
-                (data.points as any) + (!data.voteStatus ? 1 : 2) * value;
-              cache.writeFragment(
+  return {
+    url: "http://localhost:4000/graphql",
+    fetchOptions: {
+      credentials: "include" as const,
+      headers: cookie
+        ? {
+            cookie,
+          }
+        : undefined,
+    },
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        keys: {
+          PaginatedPosts: () => null,
+        },
+        resolvers: {
+          Query: {
+            posts: cursorPagination(),
+          },
+        },
+        updates: {
+          Mutation: {
+            vote: (_result, args, cache, info) => {
+              const { postId, value } = args as VoteMutationVariables;
+              const data = cache.readFragment(
                 gql`
-                  fragment __ on Post {
+                  fragment _ on Post {
+                    id
                     points
                     voteStatus
                   }
                 `,
-                { id: postId, points: newPoints, voteStatus: value } as any
+                { id: postId } as any
               );
-            }
-          },
-          createPost: (_result, args, cache, info) => {
-            const allFields = cache.inspectFields("Query");
-            const fieldInfos = allFields.filter(
-              (info) => info.fieldName === "posts"
-            );
-            fieldInfos.forEach((fi) => {
-              cache.invalidate("Query", "posts", fi.arguments || {});
-            });
-            cache.inspectFields("Query");
-          },
-          logout: (_result, args, cache, info) => {
-            betterUpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            );
-          },
-          login: (_result, args, cache, info) => {
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.error) {
-                  return query;
-                } else {
-                  return {
-                    me: result.login.user,
-                  };
+
+              if (data) {
+                if (data.voteStatus === value) {
+                  return;
                 }
+                const newPoints =
+                  (data.points as any) + (!data.voteStatus ? 1 : 2) * value;
+                cache.writeFragment(
+                  gql`
+                    fragment __ on Post {
+                      points
+                      voteStatus
+                    }
+                  `,
+                  { id: postId, points: newPoints, voteStatus: value } as any
+                );
               }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            betterUpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.error) {
-                  return query;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
+            },
+            createPost: (_result, args, cache, info) => {
+              const allFields = cache.inspectFields("Query");
+              const fieldInfos = allFields.filter(
+                (info) => info.fieldName === "posts"
+              );
+              fieldInfos.forEach((fi) => {
+                cache.invalidate("Query", "posts", fi.arguments || {});
+              });
+              cache.inspectFields("Query");
+            },
+            logout: (_result, args, cache, info) => {
+              betterUpdateQuery<LogoutMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                () => ({ me: null })
+              );
+            },
+            login: (_result, args, cache, info) => {
+              betterUpdateQuery<LoginMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                (result, query) => {
+                  if (result.login.error) {
+                    return query;
+                  } else {
+                    return {
+                      me: result.login.user,
+                    };
+                  }
                 }
-              }
-            );
+              );
+            },
+            register: (_result, args, cache, info) => {
+              betterUpdateQuery<RegisterMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                (result, query) => {
+                  if (result.register.error) {
+                    return query;
+                  } else {
+                    return {
+                      me: result.register.user,
+                    };
+                  }
+                }
+              );
+            },
           },
         },
-      },
-    }),
-    errorExchange,
-    ssrExchange,
-    fetchExchange,
-  ],
-});
+      }),
+      errorExchange,
+      ssrExchange,
+      fetchExchange,
+    ],
+  };
+};
