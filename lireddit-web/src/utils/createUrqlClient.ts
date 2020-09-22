8df4b40612428b001a88efad6f5d70546e3f4669
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-22 00:53:13 -0300

    write fragments and vote status

--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -9,6 +9,7 @@ import {
 import { pipe, tap } from "wonka";
 import { MeDocument } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import gql from "graphql-tag";
 
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
   return pipe(
@@ -134,13 +135,45 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                  voteStatus
+                }
+              `,
+              { id: postId } as any
+            );
+
+            if (data) {
+              if (data.voteStatus === value) {
+                return;
+              }
+              const newPoints =
+                (data.points as any) + (!data.voteStatus ? 1 : 2) * value;
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                    voteStatus
+                  }
+                `,
+                { id: postId, points: newPoints, voteStatus: value } as any
+              );
+            }
+          },
           createPost: (_result, args, cache, info) => {
-            const allFields = cache.inspectFields('Query');
-            const fieldInfos = allFields.filter((info) => info.fieldName === 'posts');
+            const allFields = cache.inspectFields("Query");
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === "posts"
+            );
             fieldInfos.forEach((fi) => {
               cache.invalidate("Query", "posts", fi.arguments || {});
-            })
-            cache.inspectFields('Query')
+            });
+            cache.inspectFields("Query");
           },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
