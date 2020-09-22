7d4eb7c3c459b3aa53027b118e07cfb03c318814
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-22 00:27:33 -0300

    change vote

--- lireddit-web/src/components/UpdootSection.tsx
+++ lireddit-web/src/components/UpdootSection.tsx
@@ -8,7 +8,7 @@ interface UpdootSectionProps {
 export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
   const [loadingState, setLoadingState] = useState<
     "updoot-loading" | "downdoot-loading" | "not-loading"
-  >();
+  >("not-loading");
   const [, vote] = useVoteMutation();
 
   return (
